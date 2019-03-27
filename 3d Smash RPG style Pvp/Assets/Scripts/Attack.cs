1f0abc246a34fc54f10e11199707c2fea05305c6
Thulium Games <32959306+ThuliumGames@users.noreply.github.com>
2019-03-27 09:59:16 -0400

    A New Game

---
+++ 3d Smash RPG style Pvp/Assets/Scripts/Attack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+	
+	public Transform ECol;
+	
+	float Pow;
+	
+	void OnEnable () {
+		GetComponentInParent<Move>().Anim.SetBool ("Swing", false);
+		GetComponentInParent<Move>().Anim.SetBool ("Swing2", false);
+	}
+	
+	void Update () {
+		if (SSInput.B[GetComponentInParent<Move>().Player] == "Pressed") {
+			GetComponentInParent<Move>().Anim.SetBool ("Swing", true);
+		}
+		if (SSInput.A[GetComponentInParent<Move>().Player] == "Pressed") {
+			GetComponentInParent<Move>().Anim.SetBool ("Swing2", true);
+		}
+	}
+	
+	void OnTriggerEnter (Collider C) {
+		print (C);
+		if (C.transform == ECol.transform) {
+			Pow = 10;
+			if (GetComponentInParent<Move>().Anim.GetBool ("Swing2")) {
+				Pow = 15;	
+			}
+			ECol.GetComponentInChildren<Move>().enabled = false;
+			Invoke ("Reen", 1);
+			GetComponentInChildren<BoxCollider>().enabled = false;
+		}
+	}
+	
+	void Reen () {
+		ECol.GetComponentInChildren<Move>().enabled = true;
+	}
+}
---
+++ 3d Smash RPG style Pvp/Assets/Scripts/CamCon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCon : MonoBehaviour {
+	
+	public Transform[] Gs;
+	
+	void Update () {
+		//transform.LookAt ((Gs[0].position+Gs[1].position)/2);
+		//transform.position = new Vector3 (0, Mathf.Clamp (Vector3.Distance (Gs[0].position, Gs[1].position), 5, 500), 0);
+		transform.position = ((Gs[0].position+Gs[1].position)/2);
+		transform.Translate (0, 0, Mathf.Clamp (-Vector3.Distance(Gs[0].position,Gs[1].position), -100000, -5));
+	}
+}
