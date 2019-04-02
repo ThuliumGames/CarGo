28a77a39d1ad1be88f3a5b111f7bb946bfc02271
Thulium Games <32959306+ThuliumGames@users.noreply.github.com>
2019-04-02 08:56:41 -0400

    Health

--- 3d Smash RPG style Pvp/Assets/Scripts/Attack.cs
+++ 3d Smash RPG style Pvp/Assets/Scripts/Attack.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Attack : MonoBehaviour {
 	
 	public Transform ECol;
 	
+	bool CanMH;
+	bool HA;
 	float Pow;
+	
+	public Text t;
+	public int Health = 10;
 	
 	void OnEnable () {
 		GetComponentInParent<Move>().Anim.SetBool ("Swing", false);
@@ -20,14 +26,40 @@ public class Attack : MonoBehaviour {
 		if (SSInput.A[GetComponentInParent<Move>().Player] == "Pressed") {
 			GetComponentInParent<Move>().Anim.SetBool ("Swing2", true);
 		}
+		
+		if (Health <= 0) {
+			if (GetComponentInParent<Move>().Player == 0) {
+				Application.LoadLevel("End1");
+			}
+			if (GetComponentInParent<Move>().Player == 1) {
+				Application.LoadLevel("End2");
+			}
+		}
+		
+		t.text = ""+Health;
+		
+		if (!GetComponentInParent<Move>().enabled) {
+			if (CanMH) {
+				if (HA) {
+					Health -= 2;
+				} else {
+					--Health;
+				}
+				CanMH = false;
+			}
+		} else {
+			CanMH = true;
+		}
 	}
 	
 	void OnTriggerEnter (Collider C) {
 		print (C);
 		if (C.transform == ECol.transform) {
 			Pow = 7;
+			HA = false;
 			if (GetComponentInParent<Move>().Anim.GetBool ("Swing2")) {
 				Pow = 15;
+				HA = true;
 			}
 			ECol.GetComponentInChildren<Move>().enabled = false;
 			ECol.Translate (0, 0.6f, 0);
